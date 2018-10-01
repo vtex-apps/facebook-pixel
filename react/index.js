23caa0411f42f8ae5aca173fbf1222fb33bd6046
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-10-01 17:30:39 -0300

    Add scripts boilerplates

--- react/index.js
+++ react/index.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Helmet, withRuntimeContext } from 'render'
 import { Pixel } from 'vtex.store'
 
+import { fbjs, noScript } from './scripts/fbjs'
+
+const APP_LOCATOR = 'vtex.facebook-pixel'
+
 class FacebookPixel extends Component {
+  static propTypes = {
+    subscribe: PropTypes.func,
+    runtime: PropTypes.shape({
+      workspace: PropTypes.string,
+      account: PropTypes.string,
+    }),
+  }
+
+  constructor(props) {
+    super(props)
+    this.unsubscribe = props.subscribe(this)
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe()
+    }
+  }
+
   render() {
-    return null
+    const { pixelId } = this.context.getSettings(APP_LOCATOR) || {}
+
+    if (!pixelId) {
+      const { runtime: { workspace, account } } = this.props
+      console.warn(
+        `No pixel id is defined. Take a look at https://${workspace}--${account}.myvtex.com/admin/apps/${APP_LOCATOR}/setup`
+      )
+
+      return null
+    }
+
+    const scripts = [{
+      type: 'application/javascript',
+      innerHtml: fbjs(pixelId),
+    }]
+    const noScripts = [{
+      id: 'fbjs_frame',
+      innerHtml: noScript(pixelId),
+    }]
+
+    return <Helmet script={scripts} noscript={noScripts} />
   }
 }
 
-export default Pixel(FacebookPixel)
+export default Pixel(withRuntimeContext(FacebookPixel))
