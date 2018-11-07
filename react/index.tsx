90801674c4b3eb302b2e0fa48cb1f894cc582f7e
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-07 12:29:13 -0300

    Move event listener to message

--- react/index.tsx
+++ react/index.tsx
@@ -2,38 +2,42 @@ import { formatSearchResultProducts, getProductPrice } from './utils/formatHelpe
 
 const currency = __RUNTIME__.culture.currency
 
-const track = (eventName: string, data: any) => {
-  fbq('track', eventName, data)
-}
-
-const trackCategoryPage = (page: string) => {
-  return (e: Event) => {
-    track('ViewContent', {
-      ...formatSearchResultProducts(e.data.products),
-      content_category: page,
-      content_type: 'product_group',
-      currency,
-    })
-  }
-}
-
-window.addEventListener('vtex:departmentView', trackCategoryPage('department'))
-
-window.addEventListener('vtex:categoryView', trackCategoryPage('category'))
-
-window.addEventListener('vtex:internalSiteSearchView', e => {
-  track('Search', formatSearchResultProducts(e.data.products))
-})
-
-window.addEventListener('vtex:productView', e => {
-  const { product: { productName, productId } } = e.data
-
-  track('ViewContent', {
-    content_category: 'product',
-    content_ids: [productId],
-    content_name: name,
-    content_type: 'product',
+const trackCategoryPage = (page: string, e: Event) =>
+  fbq('track', 'ViewContent', {
+    ...formatSearchResultProducts(e.data.products),
+    content_category: page,
+    content_type: 'product_group',
     currency,
-    value: getProductPrice(e.data.product),
   })
+
+window.addEventListener('message', e => {
+  switch (e.data.eventName) {
+    case 'vtex:categoryView': {
+      trackCategoryPage('category', e)
+      break
+    }
+    case 'vtex:departmentView': {
+      trackCategoryPage('department', e)
+      break
+    }
+    case 'vtex:internalSiteSearchView': {
+      fbq('track', 'Search', formatSearchResultProducts(e.data.products))
+      break
+    }
+    case 'vtex:productView': {
+      const { product: { productName, productId } } = e.data
+
+      fbq('track', 'ViewContent', {
+        content_category: 'product',
+        content_ids: [productId],
+        content_name: name,
+        content_type: 'product',
+        currency,
+        value: getProductPrice(e.data.product),
+      })
+      break
+    }
+    default:
+      break
+  }
 })
