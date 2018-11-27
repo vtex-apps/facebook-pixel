095016a9f441f556b587bb14840684318733e482
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-27 09:45:38 -0300

    Remove runtime types definition

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -1,13 +1,5 @@
 declare function fbq(s: string, t: string, o: any): void
 
-interface RuntimeContext {
-  culture: {
-    currency: string
-  }
-}
-
-declare const __RUNTIME__ : RuntimeContext
-
 interface Event extends Event {
   data: any
 }
